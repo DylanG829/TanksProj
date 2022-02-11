bf15eb58570e7076f684acf9446aeb97de5a34bd
Dylan Goldberg <dylangoldberg298@gmail.com>
2022-02-11 14:21:56 -0500

    created the display border... I think

--- tanks.py
+++ tanks.py
@@ -3,7 +3,26 @@ import random
 
 pygame.init()
 
-display_width = 800
-display_height = 600
+game_display = pygame.display.set_mode((800, 600))
 
-gameDisplay =pygame.display.set_mode(display_width, display_height)
+pygame.display.set_caption('Tanks - Brought To You By Dylan Goldberg')
+
+
+clock = pygame.time.Clock()
+
+#tanks dementions
+tankWidth = 45
+tankHeight = 25
+
+turretWidth = 6
+wheelWidth = 5
+
+ground_height = 30
+
+#fonts
+font1 = pygame.font.SysFont("Yu Mincho Demibold", 20)
+font2 = pygame.font.SysFont("lucidaconsole", 25)
+font3 = pygame.font.SysFont("comicsansms", 50)
+font4 = pygame.font.SysFont("Yu Mincho Demibold", 85)
+
+#
